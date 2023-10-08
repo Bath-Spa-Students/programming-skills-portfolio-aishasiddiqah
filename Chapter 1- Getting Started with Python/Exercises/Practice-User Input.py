ab9bbc31285ee2d2a7529242665554fa8146cb17
aishasiddiqah <aishasalam205@gmail.com>
2023-10-08 18:53:53 +0400

    Update Practice-User Input.py

--- Chapter 1- Getting Started with Python/Exercises/Practice-User Input.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice-User Input.py
@@ -1,12 +1,12 @@
 #Create a personal identification profile with user input
 
 name=(input("Enter your name:"))
-age=(input("Enter your age:"))
-number=(input("Enter your contact number:"))
+age=int(input("Enter your age:"))
+number=int(input("Enter your contact number:"))
 father=(input("Enter your father's name:"))
 mother=(input("Enter your mother's name:"))
 address=(input("Enter your address:"))
-siblings=(input("Enter the number of siblings you have:"))
+siblings=int(input("Enter the number of siblings you have:"))
 food=(input("Enter your favourite food:"))
 drink=(input("Enter your favourite drink:"))
 hobby=(input("Enter your favourite hobby:"))
