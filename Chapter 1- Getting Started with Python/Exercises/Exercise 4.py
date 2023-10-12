b8689e7ce515efb8bb60c22baada7b8a1ac4e018
aishasiddiqah <aishasalam205@gmail.com>
2023-10-12 22:53:53 +0400

    Update Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -0,0 +1,9 @@
+#Strings Concatination
+#Write three strings in different variables and print the output as one string.
+
+str_1= "Welcome,"
+str_2= "how may I"
+str_3= "help you?"
+print(str_1 , str_2 , str_3)
+
+#can also substitute (,) with (+)
