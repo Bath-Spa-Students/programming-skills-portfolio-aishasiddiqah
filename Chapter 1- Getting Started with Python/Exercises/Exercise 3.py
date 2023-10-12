d87e56ce722c70091e2ef5de674695982c0fdf3f
aishasiddiqah <aishasalam205@gmail.com>
2023-10-12 21:45:41 +0400

    Update Exercise 3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 3.py
@@ -0,0 +1,7 @@
+#Write a Python program to display the current date and time.
+
+import datetime
+
+#to get the current date and time:
+Current_date_and_time= datetime.datetime.now()
+print(Current_date_and_time)
