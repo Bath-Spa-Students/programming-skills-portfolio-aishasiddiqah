c7a0fac4b97563e188632484688f07e27d8574a7
aishasiddiqah <aishasalam205@gmail.com>
2023-10-12 22:54:12 +0400

    Update Practice Ex-3.py

--- Chapter 1- Getting Started with Python/Exercises/Practice Ex-3.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice Ex-3.py
@@ -1,4 +1,4 @@
-#Make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate linefollowed by a dot
+#Make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot
 print('''1.
 2.
 3.
