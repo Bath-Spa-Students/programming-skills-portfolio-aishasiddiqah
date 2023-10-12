2adeca0d2b2cc9ec7af2d9603c1eb71afeb36444
aishasiddiqah <aishasalam205@gmail.com>
2023-10-12 22:54:17 +0400

    Update Practice Ex-4.py

--- Chapter 1- Getting Started with Python/Exercises/Practice Ex-4.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice Ex-4.py
@@ -1,18 +1,11 @@
 #Compute the area of triangle.
-#The three sides of a triangle are a, b and c
-#The measurements of the three sides of the triangle are:
-a=float(input('Enter first side:'))
-b=float(input('Enter second side:'))
-c=float(input('Enter third side:'))
 
-#Calculate the semi-perimeter of the triangle:
-#Formula for semi-perimeter of a triangle is
-s=(a+b+c)/2
+#user input required for calculating area of triangle
+b=int(input('Enter value of base of triangle:'))
+h=int(input('Enter value of height of triangle:'))
 
 #Calculate the area of triangle:
-#Formula for finding area of triangle
-area=(s*(s-a)*(s-b)*(s-c))**0.5
+#Formula for finding the area of a triangle:
+area=(b*h)/2
 print(area)
 
-#Calculate the area of the triangle
-#Enter the values for the three sides when the program is running
