48273c69b1b4866dcc9dd54789b0c81255b70f54
aishasiddiqah <aishasalam205@gmail.com>
2023-10-19 17:45:28 +0400

    Create Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+#Your output should look something like the following, including the quotation marks:
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.
+
+print ("In his play 'Romeo and Juliet,' William Shakespeare wrote,")
+print("''What's in a name? That which we call a rose by any other name would smell just as sweet.''")
