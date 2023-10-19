60236885bb612a6114ebfc07c7e636ce08dd99a8
aishasiddiqah <aishasalam205@gmail.com>
2023-10-19 17:37:30 +0400

    Create Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+#Assign a message to a variable, and print that message. 
+#Then change the value of the variable to a new message, and print the new message.
+
+a = "Hi, how have you been lately?"
+print(a)
+
+a = "I hope everything is fine."
+print(a)
+
+a = "It's been a long time since we last saw each other."
+print(a)
