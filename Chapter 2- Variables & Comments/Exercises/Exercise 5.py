7765b32e1df53061b33d26349f126164ba286c87
aishasiddiqah <aishasalam205@gmail.com>
2023-10-22 20:39:22 +0400

    Update Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,12 @@
+#A girl heads to a computer shop to buy some USB sticks. 
+#She loves USB sticks and wants as many as she can get for £50. They are £6 each.
+#Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+#You will to use the arithmetic operators to complete this exercise.
+
+#£50 = ? USBs
+#£6 = 1 USB
+a=50/6
+print (int(a))
+b=8*6
+print(b)
+print(50-b)
