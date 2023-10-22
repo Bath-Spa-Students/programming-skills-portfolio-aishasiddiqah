068bbea02337f95487727d7e54ac98c981c4104d
aishasiddiqah <aishasalam205@gmail.com>
2023-10-22 21:18:06 +0400

    Update Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,12 @@
+#Start with the list you used in Exercise 1, but instead of just
+#printing each person’s name, print a message to them. 
+#The text of each message should be the same, but each message should be personalized 
+#with the person’s name.
+
+names= ["Esha","Fatima","Laraib","Mehreen","Sundas","Zahra"]
+print("Have a great day", names[0] + ".")
+print("Have a great day", names[1] + ".")
+print("Have a great day", names[2] + ".")
+print("Have a great day", names[3] + ".")
+print("Have a great day", names[4] + ".")
+print("Have a great day", names[5] + ".")
