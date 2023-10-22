60c958dbac3bd9184b1303c27850de4792fa9fa5
aishasiddiqah <aishasalam205@gmail.com>
2023-10-22 21:48:28 +0400

    Update Exercise 3.py

--- Chapter 3- Data Structures/Exercises/Exercise 3.py
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -1,5 +1,10 @@
 #Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
 #Use your list to print a series of statements about these items, 
 #such as “I would like to own a Honda motorcycle.”
-msg = "Hello, " + names[0].title() + "!"
-print(msg)
+
+transport=["train","car","motorcycle","bus","airplane"]
+print("I would like to sit in a", transport[-5], "and enjoy the scenery through the window.")
+print("I am learning how to drive a", transport[-4]+".")
+print("I commute by", transport[-2], "to get to the university.")
+print("I would really like to learn how to ride a", transport[-3] + ".")
+print("I enjoy travelling in an", transport[-1] + ".")
