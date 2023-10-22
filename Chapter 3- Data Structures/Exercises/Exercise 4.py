cf4dfd9cadfba487eb163d3f945e70d90e24117c
aishasiddiqah <aishasalam205@gmail.com>
2023-10-22 22:21:27 +0400

    Update Exercise 4.py

--- Chapter 3- Data Structures/Exercises/Exercise 4.py
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -2,10 +2,10 @@
 #Make a list that includes at least three people you’d like to invite to dinner. 
 #Then use your list to print a message to each person, inviting them to dinner.
 
-guest_list=["Ms.Alice","Mr.Rabbit","Mr.Mad Hatter"]
+guest_list=["Mr.Rabbit","Ms.Alice","Mr.Mad Hatter"]
 invite_1= f'I would like to cordially invite {guest_list[2]} to dinner.'
-invite_2= f'I would like to cordially invite {guest_list[1]} to dinner.'
-invite_3=f'Dear {guest_list[0]}, I would like to cordially invite you to dinner.'
+invite_2= f'I would like to cordially invite {guest_list[0]} to dinner.'
+invite_3=f'Dear {guest_list[1]}, I would like to cordially invite you to dinner.'
 end="I sincerely hope you will be able to attend. "
 regards="\nThank you."
 print(invite_1 , end , regards)
