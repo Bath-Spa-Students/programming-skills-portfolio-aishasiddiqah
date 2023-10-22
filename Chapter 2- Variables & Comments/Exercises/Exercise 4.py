b853ba80e113f0279675292f4ea82dee5afa2e71
aishasiddiqah <aishasalam205@gmail.com>
2023-10-22 20:33:13 +0400

    Update Exercise 4.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -2,7 +2,7 @@
 #Then,using that variable, create a message that reveals your favorite number. 
 #Print that message.
 
-#Using the f
+#Using the f string method:
 var_1= 21
 msg_1= f'My favourite number is {var_1}.'
 print(msg_1)
@@ -10,3 +10,10 @@ print(msg_1)
 var_2 = 7*3
 msg_2= f'My favourite number is {var_2}.'
 print(msg_2)
+
+var_3=[53,75,21,64,12,24]
+msg_3= f'My favourite number from the list is {var_3[2]}.'
+print(msg_3)
+
+msg_4= f'My favourite number from this list is {var_3[-4]}.'
+print(msg_4)
