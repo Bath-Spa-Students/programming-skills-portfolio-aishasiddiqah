51c1e60f2a4dcdf89b8da20e85e2bd2edf1113c9
aishasiddiqah <aishasalam205@gmail.com>
2023-11-02 11:52:56 +0400

    Update Practice Ex-5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice Ex-5.py
@@ -0,0 +1,8 @@
+#Write a python program that stores fruit names in a list named fruits. 
+#Unpack the list into three variables x, y and z and then print the values of each variable.
+
+#make a list named 'fruits' that contains names of fruits
+fruits=["apple","mango","banana","orange","pineapple"]
+
+x=fruits[0]
+print(x)
