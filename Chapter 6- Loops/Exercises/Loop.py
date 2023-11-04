0bc182d6174874d8d7ec02fc028344b2f97e49ec
aishasiddiqah <aishasalam205@gmail.com>
2023-11-04 14:50:45 +0400

    Update Loop.py

--- Chapter 6- Loops/Exercises/Loop.py
+++ Chapter 6- Loops/Exercises/Loop.py
@@ -3,8 +3,17 @@
 i=1
 while i<10:
     print(i)
+    i += 1 
     
 count = 1
 while count <= 5:
     print("Count is:",count)
     count += 1
+
+j=1
+while j<6:
+    print(j)
+    if j==3:
+        break
+    j += 1
+
