6e47ddb874228809c787bc0678b39ff06d7bc9bc
aishasiddiqah <aishasalam205@gmail.com>
2023-11-06 11:30:19 +0400

    Update Exercise 4.py

--- Chapter 4- Control Flow/Exercises/Exercise 4.py
+++ Chapter 4- Control Flow/Exercises/Exercise 4.py
@@ -7,8 +7,34 @@
 #•If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 #•If the person is age 65 or older, print a message that the person is an elder.
 
-age=2
+age=48
+
 if age < 2:
     print("You are a baby.")
-elif age == 2 + age < 4:
+elif age>=2 and age < 4:
     print("You are a toddler.")
+elif age>=4 and age < 13:
+    print("You are a kid.")
+elif age>=13 and age < 20:
+    print("You are a teenager.")
+elif age>=20 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are an elder.")
+
+#OR
+
+age=int(input("My age is: "))
+
+if age < 2:
+    print("You are a baby.")
+elif age>=2 and age < 4:
+    print("You are a toddler.")
+elif age>=4 and age < 13:
+    print("You are a kid.")
+elif age>=13 and age < 20:
+    print("You are a teenager.")
+elif age>=20 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are an elder.")
