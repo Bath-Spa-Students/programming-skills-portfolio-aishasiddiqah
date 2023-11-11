5cd54777b6bc9e253e4cd807fe42e88d3a14cb3c
aishasiddiqah <aishasalam205@gmail.com>
2023-11-11 19:50:52 +0400

    Update Exercise 2.py

--- Chapter 5- Dictionaries/Exercises/Exercise 2.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -8,5 +8,16 @@
 #You might print the word followed by a colon and then its meaning, or print the word on one line and then print its meaning indented on a second line. 
 #Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
 
-glossary = {}
-#5 words== variable, list, float, int, string, input, 
+glossary = {'variable' : 'a quantity used to store data',
+            'float' : 'a number that contains a decimal point',
+            'integer' : 'a number that does not contain a decimal point',
+            'input' : 'a function where a user enters data',
+            'tuple' : 'a data type which is similar to lists, but is immutable'
+            }
+print("variable:"+"\n",glossary['variable'])
+print("\n""float:"+"\n",glossary['float'])
+print("\n""integer:"+"\n",glossary['integer'])
+print("\n""input:"+"\n",glossary['input'])
+print("\n""tuple:"+"\n",glossary['tuple'])
+
+
