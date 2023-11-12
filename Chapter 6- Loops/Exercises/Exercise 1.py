fddf5fef70c07dbf14d573f85addacc41f95c292
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 19:41:05 +0400

    Update Exercise 1.py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -1,6 +1,14 @@
 #Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. 
 #As they enter each topping, print a message saying you’ll add that topping to their pizza.
 
-while True:
-    topping=input("What topping would you like on your pizza?\n")
+topping = "What toppings would you like to add on your pizza?\n"
+while topping != "quit":
+    topping = input ()
+print("Thank you! Please wait while we get your order ready.")
 
+ 
+#password = "SecretWord"
+#guess = input()
+#while guess != password:  
+  #guess = input() 
+#print("Access Granted") 
