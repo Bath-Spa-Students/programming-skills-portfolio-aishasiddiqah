8de21fa1020becb2fab6aefa9ef0bb2dab0a2d8c
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 17:15:22 +0400

    Update Exercise 4.py

--- Chapter 5- Dictionaries/Exercises/Exercise 4.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 4.py
@@ -4,3 +4,22 @@
 #* Use a loop to print a sentence about each river, such as The Nile runs through Egypt.
 #* Use a loop to print the name of each river included in the dictionary.
 #* Use a loop to print the name of each country included in the dictionary.
+
+#3 major rivers and the country each river runs through
+major_rivers = {'Euphrates':'Turkey',
+          'Rhine':'Switzerland',
+          'Nile':'Egypt'}
+
+#loop to print a sentence about each river
+for river, country in major_rivers.items():
+    print("The",river,"River", "runs through",country+".","\n")
+
+#loop to print the name of each river in the dictionary major_rivers:
+print("The rivers included within the dictionary are:" )
+for river in major_rivers.keys():
+    print(river, "River")
+
+#loop to print the name of each country included in the dictionary:
+print("\nThe following countries are included within the dictionary:")
+for country in major_rivers.values():
+    print(country)
