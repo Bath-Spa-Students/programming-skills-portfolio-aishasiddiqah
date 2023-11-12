784fa8a4c7009d14d298d1311006fbf1475167f8
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 22:28:36 +0400

    Update Exercise 1.py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -2,9 +2,8 @@
 #As they enter each topping, print a message saying you’ll add that topping to their pizza.
 
 topping = "What toppings would you like to add on your pizza?\n"
-print(topping)
 while topping != "quit":
-  topping = input ("\n"+topping + " will be added to your pizza. \nWhat other topping would you like on your pizza?\n")
+  topping = input ("What toppings would you like to add to your pizza order?\n")
 print("Thank you! Please wait while we get your order ready.")
 
  
