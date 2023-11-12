34da935a89673f46098954658b83735eb75f682f
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 18:44:02 +0400

    Update Exercise 1.py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -1,2 +1,5 @@
 #Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. 
 #As they enter each topping, print a message saying you’ll add that topping to their pizza.
+
+topping=input("What topping would you like on your pizza?\n")
+topping == ("\n(Enter 'quit' when you're finished entering toppings) ")
