00c4198129bffed3670bf9b046897ba158df9e5f
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 09:59:06 +0400

    Update Exercise 3.py

--- Chapter 5- Dictionaries/Exercises/Exercise 3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -2,3 +2,13 @@
 #calls with a loop that runs through the dictionary’s keys and values. 
 #When you’re sure that your loop works, add five more Python terms to your glossary.
 #When you run your program again, these new words and meanings should automatically be included in the output.
+
+glossary = {'variable' : 'a quantity used to store data',
+            'index' : 'the numerical value used to detect the position of an object within a list, tuple or even a string',
+            'integer' : 'a number that does not contain a decimal point',
+            'input' : 'a function where the user is required to enter data',
+            'tuple' : 'a data type which is similar to lists and stores multiple data values, but is immutable'
+            }
+
+# print the words and their meanings
+
