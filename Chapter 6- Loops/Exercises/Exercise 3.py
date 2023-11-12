7b6b72561c106c9c5934495da02e70a3e1f154eb
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 22:28:55 +0400

    Update Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,5 +1,2 @@
 #Write a loop that never ends, and run it. 
 #(To end the loop, press ctrl-C or close the window displaying the output.)
-vehicle = 'airplane'
-vehicle[0:3] = 'water'
-print(vehicle)
