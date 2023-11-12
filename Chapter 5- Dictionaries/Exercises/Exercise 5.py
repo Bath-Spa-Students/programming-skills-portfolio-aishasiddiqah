c3b8031e43bd31c804131f048468d4e5901acd16
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 17:33:25 +0400

    Update Exercise 5.py

--- Chapter 5- Dictionaries/Exercises/Exercise 5.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 5.py
@@ -2,3 +2,45 @@
 #In each dictionary, include the kind of animal and the owner’s name. 
 #Store these dictionaries in a list called pets. 
 #Next, loop through your list and as you do, print everything you know about each pet.
+
+#pet dictionaries
+#for pet_1:
+pet_1 = {'Name of pet':'',
+         'Name of owner':'',
+         'Kind of animal':'',
+         'Age of the pet':'',
+         'Colour of the pet':'',
+         'Any specific allergies':''}
+
+#for pet_2:
+pet_2 = {'Name of pet':'',
+         'Name of owner':'',
+         'Kind of animal':'',
+         'Age of the pet':'',
+         'Colour of the pet':'',
+         'Any specific allergies':''}
+
+#for pet_3:
+pet_3 = {'Name of pet':'',
+         'Name of owner':'',
+         'Kind of animal':'',
+         'Age of the pet':'',
+         'Colour of the pet':'',
+         'Any specific allergies':''}
+
+#for pet_4:
+pet_4 = {'Name of pet':'',
+         'Name of owner':'',
+         'Kind of animal':'',
+         'Age of the pet':'',
+         'Colour of the pet':'',
+         'Any specific allergies':''}
+
+#make a list called 'pets' and store all the pet dictionaries in that list
+pets = [pet_1,pet_2,pet_3,pet_4]
+
+#loop through the list and print all the information about each pet
+for pet in pets:
+    print (pet)
+   
+    
