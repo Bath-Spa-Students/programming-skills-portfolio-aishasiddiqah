1a0a604d5f466079b371dbdbe4559d1e61021667
aishasiddiqah <aishasalam205@gmail.com>
2023-11-12 16:42:24 +0400

    Update Exercise 3.py

--- Chapter 5- Dictionaries/Exercises/Exercise 3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -8,11 +8,11 @@ glossary = {'variable' : 'a quantity used to store data',
             'integer' : 'a number that does not contain a decimal point',
             'input' : 'a function where the user is required to enter data',
             'tuple' : 'a data type which is similar to lists and stores multiple data values, but is immutable',
-            'list' : '',
-            'loop' : '',
-            'float' : 'a number containing a decimal',
-            'sorted' : '',
-            'reverse' : ''
+            'list' : 'a data type containing multiple values under a single variable',
+            'while loop' : 'used in order to repeat code, but only until the condition is satisfied ',
+            'float' : 'a number that contains a decimal',
+            'sorted' : 'a function used to arrange elements in a specific order, whether in ascending or descending order, or even alphabetically',
+            'reverse' : 'a function that reverses the order of elements'
             }
 
 # print the words and their meanings
