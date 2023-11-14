545536525d1fa9d11c9faedc666373667b3b8687
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 22:06:43 +0400

    Update Practice Ex-4.py

--- Chapter 2- Variables & Comments/Exercises/Practice Ex-4.py
+++ Chapter 2- Variables & Comments/Exercises/Practice Ex-4.py
@@ -1,4 +1,12 @@
 #Write a python program that stores an integer and string value to variables x and y. 
 #Print the type of variable x and y. 
 
-#Assign an integer value to a variable named x
+#Assign an integer value to a variable named x:
+x=45
+
+#Assign a string value to a variable named y:
+y="I love to eat mangoes."
+
+#check the type of the variables
+print(type(x))
+print(type(y))
