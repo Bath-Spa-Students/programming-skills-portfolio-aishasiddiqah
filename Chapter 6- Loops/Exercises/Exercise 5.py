e83111021a1a9ded1c6578176da083b2f6e3a2e6
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 18:42:12 +0400

    Update Exercise 5.py

--- Chapter 6- Loops/Exercises/Exercise 5.py
+++ Chapter 6- Loops/Exercises/Exercise 5.py
@@ -5,11 +5,22 @@
 
 #add pastrami sandwich to the list made in the previous execise 
 sandwich_orders=["chicken sandwich","pastrami sandwich","egg sandwich","grilled cheese sandwich","pastrami sandwich","fajita sandwich","pastrami sandwich"]
+finished_sandwiches=[] 
 
 #print a message saying that the deli ran out of pasrami sandwiches
-print("We have run out of pastrami sandwiches today. Our apologies for any inconvinience caused. \nThank you for your understanding.")
+print("We have run out of pastrami sandwiches today. Our apologies for any inconvinience caused. \nThank you for your understanding.\n")
 
 #use a while loop to remove all pastrami sandwiches from list
 while 'pastrami sandwich' in sandwich_orders:
     sandwich_orders.remove("pastrami sandwich")
+
+#loop through the order list and print a message for each order that is prepared
+while sandwich_orders:
+    sandwich=sandwich_orders.pop()
+    print("Your",sandwich,"is ready. Enjoy!")
+    finished_sandwiches.append(sandwich)
+
+#print a message listing each sandwich that was made
+for sandwich in finished_sandwiches:
+    print("\tThe",sandwich,"was prepared.")
 
