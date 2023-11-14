0a9f6106bd76ab762f79721d88b6b26bf442366a
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 21:44:08 +0400

    Update Practice Ex-2.py

--- Chapter 2- Variables & Comments/Exercises/Practice Ex-2.py
+++ Chapter 2- Variables & Comments/Exercises/Practice Ex-2.py
@@ -2,5 +2,19 @@
 #After calculating the average, calculate the percentage of a student using total marks. 
 #Assume the total of all the courses marks is 500 or take the total marks from the user as input. 
 
-#Take input for courses marks
-coarse_marks= input()
+#Ask the user for the total number of courses
+courses=int(input("How many courses do you take in total?\n"))
+
+#Ask the user for the value of total marks
+total=int(input("What are the total marks in all the subjects combined?\n"))
+
+#Take input for the courses marks that the user attained
+course_marks=int(input("How much did you score in total in all the subjects?\n"))
+
+#calculate the average
+average=course_marks/courses
+print("The average is:",average)
+
+#calculate the percentage
+percentage= (course_marks/total)*100
+print("The percentage is:",percentage)
