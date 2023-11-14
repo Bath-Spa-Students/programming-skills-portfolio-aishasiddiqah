ad6b75a54b5f82e012c78a5392e20905ba6a885b
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 23:05:08 +0400

    Update Practice Ex-1.py

--- Chapter 3- Data Structures/Exercises/Practice Ex-1.py
+++ Chapter 3- Data Structures/Exercises/Practice Ex-1.py
@@ -1,15 +1,17 @@
 #Given a Python list, write a program to remove all occurrences of item 20.
 
 #Given list:
-list1 = [5, 20, 15, 20, 25, 50, 20]
+#list1 = [5, 20, 15, 20, 25, 50, 20]
 
 #using for loop:
+list1 = [5, 20, 15, 20, 25, 50, 20]
 for num in list1:
     if num==20:
         list1.remove(num)
 print(list1)
 
 #OR
+#using the pop() function:
 list1 = [5, 20, 15, 20, 25, 50, 20]
 list1.pop(6)
 list1.pop(3)
@@ -17,20 +19,11 @@ list1.pop(1)
 print(list1)
 
 #OR
+#using the remove() function:
 list1 = [5, 20, 15, 20, 25, 50, 20]
 list1.remove(20)
 list1.remove(20)
 list1.remove(20)
-print(list1)
-
-#OR
-list1 = [5, 20, 15, 20, 25, 50, 20]
-(f'{list1.remove(20)}{list1.remove(20)}{list1.remove(20)}')
-print(list1)
-
-#OR
-list1 = [5, 20, 15, 20, 25, 50, 20]
-(f'{list1.pop(6)}{list1.pop(3)}{list1.pop(1)}')
 print(list1)
 
 
