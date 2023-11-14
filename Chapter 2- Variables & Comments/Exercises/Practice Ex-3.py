15c3957cfb2fc62e673e60ddbd15bea18c3f9d4f
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 21:56:19 +0400

    Update Practice Ex-3.py

--- Chapter 2- Variables & Comments/Exercises/Practice Ex-3.py
+++ Chapter 2- Variables & Comments/Exercises/Practice Ex-3.py
@@ -1,3 +1,15 @@
 #Write a python program that takes an input 5 from user and then type cast those values to string, int
 # and float in the separate variables. 
 #Print all the variables.
+
+info=input("Write whatever number you want to: ")
+
+str_1=str(info)
+
+int_1=int(info)
+
+float_1=float(info)
+
+print(str_1)
+print(int_1)
+print(float_1)
