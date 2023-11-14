8c6af0d0022500757fb4de27c09d83c420154375
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 21:09:10 +0400

    Update Exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -7,3 +7,10 @@ str_3= "help you?"
 print(str_1 , str_2 , str_3)
 
 #can also substitute (,) with (+)
+#the only difference will be that when you use (,) there is an automatic space before the next word
+#but if (+) is used, then there is no extra space and the two are connected
+#for example
+# a=volley 
+# b=ball
+# the output of print(a,b) will be 'volley ball'
+# the output of print(a+b) will be 'volleyball'
