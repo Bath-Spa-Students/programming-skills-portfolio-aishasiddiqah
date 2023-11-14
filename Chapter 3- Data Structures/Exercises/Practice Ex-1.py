44ca2110fbe582fd00e5e51a0ad1a413a2eeda03
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 23:01:46 +0400

    Update Practice Ex-1.py

--- Chapter 3- Data Structures/Exercises/Practice Ex-1.py
+++ Chapter 3- Data Structures/Exercises/Practice Ex-1.py
@@ -2,33 +2,8 @@
 
 #Given list:
 list1 = [5, 20, 15, 20, 25, 50, 20]
-list1.pop(6)
-list1.pop(3)
-list1.pop(1)
+for num in list1:
+    if num==20:
+        list1.remove(num)
 print(list1)
-
-#OR
-list1 = [5, 20, 15, 20, 25, 50, 20]
-list1.remove(20)
-list1.remove(20)
-list1.remove(20)
-print(list1)
-
-#OR
-list1 = [5, 20, 15, 20, 25, 50, 20]
-(f'{list1.remove(20)}{list1.remove(20)}{list1.remove(20)}')
-print(list1)
-
-#OR
-list1 = [5, 20, 15, 20, 25, 50, 20]
-(f'{list1.pop(6)}{list1.pop(3)}{list1.pop(1)}')
-print(list1)
-
-#OR
-list1 = [5, 20, 15, 20, 25, 50, 20]
-while True:
-    list1.remove(20)
-    if list1!=20
-    break
-
 
