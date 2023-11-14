39eb0043ee0de50d24b9f77fd541fffc7cea7d23
aishasiddiqah <aishasalam205@gmail.com>
2023-11-14 11:19:07 +0400

    Update Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,7 +1,7 @@
 #Write a loop that never ends, and run it. 
 #(To end the loop, press ctrl-C or close the window displaying the output.)
 
-never_ending=input("The weather is very nice today, isn't it?")
+never_ending=input("The weather is very nice today, isn't it?\n")
 while True:
     print(never_ending)
     if never_ending=="yes":
