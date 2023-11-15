da3e2f8b87c9b5100fb1068f12229b047b6050c9
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 22:12:01 +0400

    Update Practice Ex-3.py

---
+++ Chapter 4- Control Flow/Exercises/Practice Ex-3.py
@@ -0,0 +1,9 @@
+#Write a python program with nested decision structures that perform the following: 
+#If amount1 is greater than 10 and amount2 is less than 100, display the greater of amount1 and amount2
+
+#amount1 > 10 and amount2 < 100, then compare the 2
+#Take user input for assigning values to variables 'amount1' and 'amount2'
+amount1=int(input("Enter a value here: "))
+amount2=int(input("Enter a value here: "))
+print("amount1=",amount1)
+print("amount1=",amount2)
