30b37d3301b30da964bdf5ab8773774e650355e1
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 23:22:58 +0400

    Update Practice Ex-5.py

--- Chapter 4- Control Flow/Exercises/Practice Ex-5.py
+++ Chapter 4- Control Flow/Exercises/Practice Ex-5.py
@@ -2,7 +2,7 @@
 #month provided as input.
 
 #take user input
-month=input("Please enter the month: ")
+month=str(input("Please enter the month: "))
 
 #for determining the season based on the month:
 # summer= june, july, august  
@@ -10,15 +10,15 @@ month=input("Please enter the month: ")
 # winter= december, january, february
 # spring= march, april, may
 
-if month=="june" or "july" or "august":
-    season="summer"
-    print("It is the",season,"season.")
-elif month=="september" or "october" or "november":
+if month=="september" or month=="october" or month=="november":
     season="autumn"
     print("It is the",season,"season.")
-elif month=="december" or "january" or "february":
+elif month=="december" or month=="january" or month=="february":
     season="winter"
     print("It is the",season,"season.")
-elif month=="march" or "april" or "may":
+elif month=="march" or month=="april" or month=="may":
     season="spring"
+    print("It is the",season,"season.")
+elif month=="june" or month=="july" or month=="august":
+    season="summer"
     print("It is the",season,"season.")
