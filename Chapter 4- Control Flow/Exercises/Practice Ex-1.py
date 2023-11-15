9b17340dbbfc88540bcdff362289f9a550b24dea
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 22:11:57 +0400

    Update Practice Ex-1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice Ex-1.py
@@ -0,0 +1,12 @@
+#Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the
+#variable z if the variable & is greater than 100.
+
+#take input from user and store in a variable called 'var'
+var=int(input("Enter a number of your choice: "))
+
+#write an if statement where:
+#if var>100, y=20 and z=40
+if var>100:
+    y=20
+    z=40
+    print("y =",y,"\nz =",z)
