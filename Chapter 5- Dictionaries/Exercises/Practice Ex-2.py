e0b62b947bae8e9eb1e4f0dbd680767ff9753d98
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 23:49:43 +0400

    Update Practice Ex-2.py

--- Chapter 5- Dictionaries/Exercises/Practice Ex-2.py
+++ Chapter 5- Dictionaries/Exercises/Practice Ex-2.py
@@ -1,1 +1,12 @@
 #Write a Python program to iterate through the keys of a dictionary and print them.
+
+myself = {"Name":"Aisha",
+      "Age":"18",
+      "Siblings":"2 siblings",
+      "Country_of_Residence":"United Arab Emirates",
+      "City":"Ajman",
+      "Nationality":"Pakistan"}
+
+#to iterate through the keys
+for key in myself.keys():
+    print(key)
