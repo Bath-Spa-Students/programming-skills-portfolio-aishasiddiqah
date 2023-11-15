09c92c627f591d42a8f730621dc67bd01ad36c04
aishasiddiqah <aishasalam205@gmail.com>
2023-11-16 00:54:34 +0400

    Update Practice Ex-1.py

---
+++ Chapter 7- Functions/Exercises/Practice Ex-1.py
@@ -0,0 +1,20 @@
+#Write a Python function that takes two numbers as arguments and returns their sum.
+
+#arguments:
+x=43
+v=634
+
+#function
+sum=x+v
+
+#print
+print(sum)
+
+#OR
+#if we take user input
+
+x=int(input("Enter value of x: "))
+v=int(input("Enter value of v: "))
+
+total=x+v
+print("The sum of the two numbers is =",total)
