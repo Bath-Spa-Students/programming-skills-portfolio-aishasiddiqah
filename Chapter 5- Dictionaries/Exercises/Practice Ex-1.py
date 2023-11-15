b0f0a976e071208db755f148b8ae2e388ef0d239
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 23:35:19 +0400

    Update Practice Ex-1.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice Ex-1.py
@@ -0,0 +1,17 @@
+#Use a dictionary to store information about yourself
+
+#make dictionary
+myself = {"Name":"Aisha",
+      "Age":"18",
+      "Siblings":"2 siblings",
+      "Country_of_Residence":"United Arab Emirates",
+      "City":"Ajman",
+      "Nationality":"Pakistan"}
+
+#print the information
+print(myself["Name"])
+print(myself["Age"])
+print(myself["Siblings"])
+print(myself["Country_of_Residence"])
+print(myself["City"])
+print(myself["Nationality"])
