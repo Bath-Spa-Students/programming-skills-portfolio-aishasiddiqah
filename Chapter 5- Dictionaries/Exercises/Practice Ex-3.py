90afbc672b33dfa8df2f2316eb7f1e76edec0c2b
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 23:49:45 +0400

    Update Practice Ex-3.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice Ex-3.py
@@ -0,0 +1,12 @@
+#Write a Python program to iterate through the values of a dictionary and print them.
+
+myself = {"Name":"Aisha",
+      "Age":"18",
+      "Siblings":"2 siblings",
+      "Country_of_Residence":"United Arab Emirates",
+      "City":"Ajman",
+      "Nationality":"Pakistan"}
+
+#to iterate through the values
+for value in myself.values():
+    print(value)
