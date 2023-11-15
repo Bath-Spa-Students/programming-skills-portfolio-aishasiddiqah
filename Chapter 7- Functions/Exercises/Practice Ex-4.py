fb406b318d5527b6d75b04b9a0de10eeef2aaa57
aishasiddiqah <aishasalam205@gmail.com>
2023-11-16 01:03:24 +0400

    Update Practice Ex-4.py

---
+++ Chapter 7- Functions/Exercises/Practice Ex-4.py
@@ -0,0 +1,7 @@
+#Write a Python program that defines a function to calculate the area of a circle, 
+#and then calls that function with a given radius.
+
+def area(radius):
+    print(f'The area of the circle is:\n{(3.14)*(radius)**2}')
+
+area(22)
