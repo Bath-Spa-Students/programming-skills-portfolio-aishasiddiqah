012c70b2755feedd2fa84720f2aed7978160ec39
aishasiddiqah <aishasalam205@gmail.com>
2023-11-15 23:49:40 +0400

    Update Practice Ex-1.py

--- Chapter 5- Dictionaries/Exercises/Practice Ex-1.py
+++ Chapter 5- Dictionaries/Exercises/Practice Ex-1.py
@@ -7,11 +7,3 @@ myself = {"Name":"Aisha",
       "Country_of_Residence":"United Arab Emirates",
       "City":"Ajman",
       "Nationality":"Pakistan"}
-
-#print the information
-print(myself["Name"])
-print(myself["Age"])
-print(myself["Siblings"])
-print(myself["Country_of_Residence"])
-print(myself["City"])
-print(myself["Nationality"])
