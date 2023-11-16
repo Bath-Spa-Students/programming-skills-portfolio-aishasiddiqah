670f9d9f8267b43299b31b06cb4e211421a540d0
aishasiddiqah <aishasalam205@gmail.com>
2023-11-16 13:38:14 +0400

    Update Practice Ex-3.py

--- Chapter 7- Functions/Exercises/Practice Ex-3.py
+++ Chapter 7- Functions/Exercises/Practice Ex-3.py
@@ -1,2 +1,16 @@
 #Write a Python program that uses a function to check if a given number is prime or not.
 
+#by using the math module
+import math
+
+#define a function called prime_num()
+def prime_num(z):
+    if z<=1:
+        return False
+    for x in range(2, int(math.sqrt(z))+1):
+        if z%x==0:
+            return False
+    return True
+
+z=int(input("Enter a number: "))
+print(prime_num(z))
