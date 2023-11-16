8be34eaae4003d70afb91cb1b84ef4685e407d64
aishasiddiqah <aishasalam205@gmail.com>
2023-11-16 13:11:18 +0400

    Update Practice Ex-2.py

--- Chapter 7- Functions/Exercises/Practice Ex-2.py
+++ Chapter 7- Functions/Exercises/Practice Ex-2.py
@@ -3,16 +3,18 @@
 #define a function named factorial
 #the function should calculate the factorial of a given positive integer using recursion
 def factorial(n):
-    if n==0 or n==1:
+    if n==1:
         return n
     else:
         return (n*factorial(n-1))
 
-#take user input to obtain the integer
-integer=(input("Enter an integer here: "))
+integer=1
 
 #since the integer has to be positive
 #check and confirm in case the number is negative
-
-
-print("The factorial of",integer,"is:\n",factorial(integer))
+if integer<0:
+    print("The factorial can only be calculated for positive numbers, not negative numbers.")
+elif integer==0:
+    print("The factorial of 0 is 1.")
+else:
+    print("The factorial of",integer,"is:\n",factorial(integer))
