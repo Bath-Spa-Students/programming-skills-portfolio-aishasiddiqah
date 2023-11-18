8acaa22479f15b315d1c39d3f05f172396211679
aishasiddiqah <aishasalam205@gmail.com>
2023-11-18 16:52:26 +0400

    Update Practice Ex-4.py

--- Chapter 5- Dictionaries/Exercises/Practice Ex-4.py
+++ Chapter 5- Dictionaries/Exercises/Practice Ex-4.py
@@ -1,6 +1,6 @@
 #Write a Python program to iterate through both the keys and values of a dictionary and print them.
 
-myself = {"Name":"Aisha",
+myself = {"Name: ":"Aisha",
       "Age: ":"18",
       "Siblings: ":"2 siblings",
       "Country_of_Residence: ":"United Arab Emirates",
